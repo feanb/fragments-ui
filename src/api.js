6a1d5b3d3a8b1ee20ddfcf51d6099f85095a7a92
Neda Basoglu <nedabsgl@gmail.com>
2022-07-17 17:29:47 -0400

    Updating Fragments-Ui for assignment 2

--- src/api.js
+++ src/api.js
@@ -1,8 +1,4 @@
-// src/api.js
-
-// fragments microservice API, defaults to localhost:8080
-//  'http://localhost:8080'
-
+// fragments microservice API
 const api = process.env.API_URL;
 
 /**
@@ -10,12 +6,13 @@ const api = process.env.API_URL;
  * fragments microservice (currently only running locally). We expect a user
  * to have an `idToken` attached, so we can send that along with the request.
  */
+export async function getUserFragments(user, expand=0) {
+  console.log('Requesting user fragments data...');
 
-export async function getUserFragments(user) {
-  console.log('Get fragments data...');
   try {
-    const res = await fetch(`${api}/v1/fragments`, {
+    const res = await fetch(`${api}/v1/fragments?expand=${expand}`, {
       headers: {
+        // Include the user's ID Token in the request so we're authorized
         Authorization: `Bearer ${user.idToken}`,
       },
     });
@@ -23,50 +20,159 @@ export async function getUserFragments(user) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
-    console.log('User fragments data', { data });
+    console.log('Got user fragments data', { data });
     return { data };
   } catch (err) {
-    console.error('Cant GET /v1/fragments', { err });
+    console.error('Unable to call GET /v1/fragments', { err });
   }
 }
 
-//Get by id
-export async function getFragmentById(user, id) {
-  console.log(`Requesting fragment by id ${id}`);
+export async function getFragmentById(user, id, ext='') {
+  console.log(`Requesting user fragment data by id ${id}`);
   try {
-    const res = await fetch(`${api}/v1/fragments/${id}`, {
+    const res = await fetch(`${api}/v1/fragments/${id}${ext}`, {
       headers: {
+        // Include the user's ID Token in the request so we're authorized
         Authorization: `Bearer ${user.idToken}`,
       },
     });
+    
     if (!res.ok) {
-      throw new Error(`${res.status} ${res.statusText}`);
+      const error = await res.json();
+      throw error.error?.message || res.statusText;
     }
-    const data = await res.json();
-    console.log('Fragment with id', { data });
-    return { data };
+
+    console.log('Got fragments data with given id', res);
+    console.log('res content type', res.headers.get('content-type'));
+
+    const contentType = res.headers.get('content-type');
+
+    if (contentType.startsWith('text/')) {
+      try {
+        return { contentType, data: await res.text() };
+      } catch (e) {
+        console.error('cannot return text fragment', { e });
+      }
+    } else if (contentType.startsWith('application/json')) {
+      try {
+        return { contentType, data: await res.json() };
+      } catch (e) {
+        console.error('cannot return json fragment', { e });
+      }      
+    } else if (contentType.startsWith('image/')) {
+      try {
+        const myBlob = await res.blob();
+        return { contentType, data: myBlob };
+      } catch (e) {
+        console.error('cannot return image blob', { e });
+      } 
+    }
   } catch (err) {
-    console.error('Cant GET /v1/fragments/:id', { err });
+    console.error('Unable to call GET /v1/fragments/:id', { err });
+    throw new Error(err);
   }
 }
 
-export async function postFragment(user, value) {
+export async function getFragmentByIdInfo(user, id) {
+  console.log(`Requesting user fragment metadata by id ${id}`);
+  try {
+    const res = await fetch(`${api}/v1/fragments/${id}/info`, {
+      headers: {
+        // Include the user's ID Token in the request so we're authorized
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    
+    if (!res.ok) {
+      const error = await res.json();
+      throw error.error?.message || res.statusText;
+    }
+
+    // console.log('Got fragments metadata with given id', res);
+
+    const data = await res.json();
+    console.log('Got fragment metadata', { data });
+    return data;
+  } catch (err) {
+    console.error('Unable to call GET /v1/fragments/:id', { err });
+    throw new Error(err);
+  }
+}
+
+/**
+ * Post fragment to the server
+ */
+export async function postFragment(user, value, contentType) {
   console.log('Post fragment data...');
   try {
     const res = await fetch(`${api}/v1/fragments`, {
-      method: "POST",
+      method: 'POST',
       headers: {
+        // Include the user's ID Token in the request so we're authorized
         Authorization: `Bearer ${user.idToken}`,
+        'Content-Type': contentType,
       },
       body: value,
     });
     if (!res.ok) {
-      throw new Error(`{res.status} ${res.statusText}`);
+      const error = await res.json();
+      throw error.error?.message || res.statusText;
     }
     const data = await res.json();
     console.log('Posted fragments data', { data });
+    return data;
   } catch (err) {
     console.error('Unable to call POST /v1/fragments', { err });
+    throw new Error(err);
   }
 }
 
+/**
+ * Put (update) fragment to the server
+ */
+export async function updateFragment(user, value, id, contentType) {
+  console.log('Update fragment data...');
+  try {
+    const res = await fetch(`${api}/v1/fragments/${id}`, {
+      method: 'PUT',
+      headers: {
+        // Include the user's ID Token in the request so we're authorized
+        Authorization: `Bearer ${user.idToken}`,
+        'Content-Type': contentType,
+      },
+      body: value,
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Updated fragments data', { data });
+    return data;
+  } catch (err) {
+    console.error('Unable to call PUT /v1/fragments', { err });
+  }
+}
+
+/**
+ * Delete fragment from the server
+ */
+export async function deleteFragment(user, id) {
+  console.log('Delete fragment data...');
+  try {
+    const res = await fetch(`${api}/v1/fragments/${id}`, {
+      method: 'DELETE',
+      headers: {
+        // Include the user's ID Token in the request so we're authorized
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Deleted fragments data', { data });
+    return data;
+  } catch (err) {
+    console.error('Unable to call DELETE /v1/fragments', { err });
+  }
+}
--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
-// src/app.js
-
 import { Auth, getUser } from './auth';
-import { getUserFragments, getFragmentById, postFragment } from './api';
+import { getUserFragments, getFragmentById, getFragmentByIdInfo, postFragment, updateFragment, deleteFragment } from './api';
 
 async function init() {
   // Get our UI elements
   const userSection = document.querySelector('#user');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
-  const fragmentSection = document.querySelector('#fragment');
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
@@ -30,35 +27,230 @@ async function init() {
     logoutBtn.disabled = true;
     return;
   }
-  getUserFragments(user);
+
   // Log the user info for debugging purposes
   console.log({ user });
+
+  await getUserFragments(user, 1);
 
   // Update the UI to welcome the user
   userSection.hidden = false;
 
   // Show the user's username
-  userSection.querySelector('.username').innerText = user.username;
-
+  userSection.querySelector('.username').innerText = user.username;  
+  
   // Disable the Login button
   loginBtn.disabled = true;
-  document.getElementById("myForm").onSubmit = () => {
-    console.log("input: " + document.getElementById("textFragment").value);
-  }
-  var myForm = document.querySelector("form");
-  myForm.addEventListener("submit", myFunction);   
 
-  //Post functionality is not workinnnnn
-  async function myFunction(e) {
+  const existingFragmentsSection = document.querySelector('#existingFragmentsSection');
+  const selectTypeSection = document.querySelector('#type');
+  const fileImportSection = document.querySelector('#fileImport');
+  const convertTypeFormSection = document.querySelector('#convertType');
+  const metadataSection = document.querySelector('#metadataSection');
+
+  const convertTypeForm = document.getElementById('convertTypeForm');
+  const img = document.getElementById('imageFragment');
+  const deleteBtn = document.getElementById('deleteBtn');
+  const fragmentToDisplay = document.getElementById('fragment');
+  const metadataToDisplay = document.getElementById('metadata');
+
+  // default feature is create
+  let selectedFeature = 'create';
+  // selected fragment for update
+  let selectedFragment = {};
+  let selectedType = 'text/plain';
+
+  // select what to do: create/update/delete
+  document.getElementById('features').addEventListener('change', async function(e) {
     e.preventDefault();
-    console.log("User entered: " + document.getElementById("inputFragment").value);
-    await postFragment(user, document.getElementById("inputFragment").value);
-    const fragment = await getUserFragments(user);
-    //use the most recently added fragment's id
-    if (fragment && fragment !== undefined) {
-      const fg = await getFragmentById(user, fragment.data.fragments[fragment.data.fragments.length -1]);
-      fragmentSection.querySelector('.fragment').innerText = fg["data"];
+    selectedFeature = e.target.value;
+    console.log('selected feature: ' + selectedFeature);
+
+    // remove fragment value displayed before
+    img.src = '';   
+    fragmentToDisplay.innerHTML = '';
+    metadataToDisplay.innerText = '';
+    metadataSection.style.display = 'none';
+
+    if (selectedFeature === 'view') {
+      convertTypeFormSection.style.display = 'inline-block';
+    } else {
+      convertTypeFormSection.style.display = 'none';
     }
+
+    if (selectedFeature === 'delete') {
+      deleteBtn.style.display = 'inline-block';
+    } else {
+      deleteBtn.style.display = 'none';
+    }
+
+    if (selectedFeature !== 'create') {
+      const fragments = await getUserFragments(user, 1);
+      document.getElementById('existingFragmentsLbl').innerText = `Choose a fragment to ${selectedFeature}`;
+      
+      removeFragmentsOptions();
+      // fill the options to select existing fragment to view/update/delete
+      await fillFragmentsOptions(fragments.data.fragments);
+      existingFragmentsSection.style.display='inline-block';
+      selectTypeSection.style.display='none';
+
+      if (selectedFeature === 'update') {
+        fileImportSection.style.display='block';
+      } else {
+        fileImportSection.style.display='none';
+      }
+    } else {
+      existingFragmentsSection.style.display='none';
+      selectTypeSection.style.display='block';
+      fileImportSection.style.display='block';
+    }
+  });
+
+  deleteBtn.onclick = async () => {
+    try {
+      const deleted = await deleteFragment(user, selectedFragment.id);
+      if (deleted.status === 'ok') {
+        fragmentToDisplay.innerHTML = `Fragment with id: ${selectedFragment.id} has been successfully deleted.`;
+      }
+      
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // fill the options to select existing fragment to view/update/delete
+  function fillFragmentsOptions(fragments) {
+    // sort by updated value: for when working with DynamoDB
+    fragments.sort(function(a,b){
+      return new Date(b.updated) - new Date(a.updated);
+    });
+
+    const select = document.getElementById('existingFragments');
+    
+    for (let i=0; i<fragments.length; i++) {
+      const option = new Option();
+      option.value = JSON.stringify({id: fragments[i].id, type: fragments[i].type});
+      option.text = `${fragments[i].id}, ${fragments[i].type}`;
+      select.appendChild(option);
+    }
+  }
+
+  // remove fragments options
+  function removeFragmentsOptions() {
+    const select = document.getElementById('existingFragments');
+    while (select.lastChild.id !== 'default') {
+      select.removeChild(select.lastChild);
+    }
+  }
+  
+  // event handler when user selects an existing fragment
+  document.getElementById('existingFragments').addEventListener('change', function(e) {
+    e.preventDefault();
+    if (e.target.value === 'Select a fragment') {
+      fragmentToDisplay.innerHTML = 'Please select a fragment';
+    } else {
+      // console.log(JSON.parse(e.target.value));
+      selectedFragment = JSON.parse(e.target.value);
+      const selectedFragmentId = selectedFragment.id;
+      selectedType = selectedFragment.type;
+      console.log('selected fragment: ' + selectedFragmentId);
+    }
+  });
+
+  // event handler when user selects to create fragment
+  document.getElementById('types').addEventListener('change', function(e) {
+    e.preventDefault();
+    selectedType = e.target.value;
+    console.log('selected type: ' + selectedType);
+  });
+
+  async function displayFragment(id, selectedConversionType) {
+    try {
+      // delete fragment displayed before
+      while (fragmentToDisplay.firstChild) {
+        fragmentToDisplay.removeChild(fragmentToDisplay.firstChild);
+      }
+      fragmentToDisplay.innerText = '';
+      img.src = '';
+
+      const { contentType, data } = await getFragmentById(user, id, selectedConversionType);
+      const metadata = await getFragmentByIdInfo(user, id);
+      if (metadata) {
+        metadataSection.style.display = 'block';
+        metadataToDisplay.innerHTML = JSON.stringify(metadata);
+      }
+      if (contentType.startsWith('image/')) {
+        img.src = URL.createObjectURL(data);  
+      } else if (contentType.startsWith('text/html')) {
+        fragmentToDisplay.insertAdjacentHTML('afterbegin',data);
+      } else {
+        fragmentToDisplay.innerText = typeof data === 'object' ? JSON.stringify(data) : data;
+      }
+    } catch (e) {
+      console.error('Get by id failed after post through file: ', { e });
+      fragmentToDisplay.innerText = e;
+      metadataSection.style.display = 'none';
+    }
+  }
+
+  convertTypeForm.addEventListener('submit', (e) => handleConvertTypeForm(e));
+
+  // for a2, only support md -> html conversion.
+  async function handleConvertTypeForm(e, id) {
+    e.preventDefault();
+    fragmentToDisplay.innerText = '';
+    const selectedConversionType = document.getElementById('convertTypes').value;
+    let fragmentId;
+    if (selectedFeature === 'view') {
+      fragmentId = selectedFragment.id;
+    } else {
+      fragmentId = id;
+    }
+    console.log('selected feature:' + selectedFeature);
+    console.log('id:' + fragmentId);
+    console.log('selected conversion extension: ' + document.getElementById('convertTypes').value);
+    //use the most recently added fragment's id
+    displayFragment(fragmentId, selectedConversionType);
+  }
+
+  const inputEl = document.getElementById('inputFile');
+  inputEl?.addEventListener('change', handleFile);
+
+  function handleFile(evt) {
+    const files = evt.target.files; // FileList object
+    const f = files[0];
+    const reader = new FileReader();    
+    fragmentToDisplay.innerText = '';
+
+    // Capture the file information
+    reader.onload = (function() {
+      return async function(e) {
+        console.log('uploaded file type: ' + f.type);
+ 
+        if (selectedType && selectedType !== f.type) {
+          alert('Uploaded file type must be same as selected type.');
+        } else {
+          console.log('selected feature: ' + selectedFeature);
+
+          let fragment;
+
+          try {
+            if (selectedFeature === 'create') {
+              fragment = await postFragment(user, e.target.result, f.type);
+            } else {
+              fragment = await updateFragment(user, e.target.result, selectedFragment.id, f.type);
+            }
+            if (fragment) {
+              fragmentToDisplay.innerHTML = `Fragment of type ${fragment.fragment.type} with id: ${fragment.fragment.id} is ${selectedFeature}d!`;
+            }
+          } catch (e) {
+            fragmentToDisplay.innerText = e;
+          }
+        }
+      };
+    })(f);
+
+    reader.readAsArrayBuffer(f);
   }
 }
 
